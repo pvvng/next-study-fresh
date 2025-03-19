7d527e43f65d6765e14a622835bb540f01f9e8fb
맥동우 <t6u80o@naver.com>
2025-03-19 19:00:46 +0900

    step3

--- app/list/page.js
+++ app/list/page.js
@@ -1,13 +1,18 @@
 export default function List() {
-  return (
-    <div>
-      <h2>Products</h2>
-      <div className="food">
-        <h4>상품명 $40</h4>
-      </div>
-      <div className="food">
-        <h4>상품명 $40</h4>
-      </div>
+  let 상품 = ['Tomatoes', 'Pasta', 'Coconut'];
+
+  return (  
+  <div> 
+      <h4 className="title">상품목록</h4>
+      {상품.map((상품명, index)=>{
+        return ( 
+          <div key={index} className="food"> 
+            <h4>{상품명} $40</h4>
+            {/* 혹은  */}
+            {/* <h4>{ 상품[인덱스] } $40</h4> */}
+          </div>
+        )
+      })}
     </div>
   )
 } 
