6857fbc63585b22b5dd56f32494fa425b7e93b4c
맥동우 <t6u80o@naver.com>
2025-03-19 18:57:52 +0900

    step2

--- app/page.js
+++ app/page.js
@@ -1,11 +1,8 @@
 export default function Home() {
-  let name = 'pvvng';
-  let link = 'https://naver.com';
   return (
-    <div>
-      <h4 className="title" style={{ color : 'red', fontSize : '20px' }}>넥스트스터디</h4>
-      <p className="title-sub">by dev {name}</p>
-      <a href={link} className="link">링크</a>
-    </div>
+    <main>
+      <h1 className="title">Programming Log</h1>
+      <p className="title-sub">by dev Pvvng</p>
+    </main>
   )
 }
