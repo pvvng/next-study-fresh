a1e78a90141fb59762c6b4bda0d7ed6ddeb25860
맥동우 <t6u80o@naver.com>
2025-03-19 19:16:33 +0900

    step8

--- app/list/page.js
+++ app/list/page.js
@@ -5,7 +5,7 @@ import { useState } from "react";
 
 export default function List() {
   let 상품 = ['Tomatoes', 'Pasta', 'Coconut'];
-  let [수량, 수량변경] = useState(0)
+  let [수량, 수량변경] = useState([0,0,0]);
 
   return (  
   <div> 
@@ -16,7 +16,11 @@ export default function List() {
             <Image src={`/food${index}.png`} alt={상품명} width={150} height={150} />
             <h4>{상품명} $40</h4>
             <span> {수량} </span>
-            <button onClick={()=>{ 수량변경(수량+1) }}>+</button>
+            <button onClick={()=>{ 
+              let copy = [...수량]
+              copy[0]++
+              수량변경(copy)
+            }}>+</button>
           </div>
         )
       })}
