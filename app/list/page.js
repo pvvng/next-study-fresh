c41a7ed9e57210895b10f934c3cdfb263368b0df
맥동우 <t6u80o@naver.com>
2025-03-19 19:14:57 +0900

    step7

--- app/list/page.js
+++ app/list/page.js
@@ -1,7 +1,11 @@
+"use client";
+
 import Image from "next/image";
+import { useState } from "react";
 
 export default function List() {
   let 상품 = ['Tomatoes', 'Pasta', 'Coconut'];
+  let [수량, 수량변경] = useState(0)
 
   return (  
   <div> 
@@ -11,6 +15,8 @@ export default function List() {
           <div key={index} className="food"> 
             <Image src={`/food${index}.png`} alt={상품명} width={150} height={150} />
             <h4>{상품명} $40</h4>
+            <span> {수량} </span>
+            <button onClick={()=>{ 수량변경(수량+1) }}>+</button>
           </div>
         )
       })}
